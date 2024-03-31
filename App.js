d63c8eb191e14b7d3c44fa1682eb78c003d12d77
JYOTIPM1999 <jyotipm1999@gmail.com>
2024-03-31 20:15:12 +0530

    Update some dependencies version

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+import { useFonts } from "expo-font";
+
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Details from "./screens/Details";
+
+const Stack = createStackNavigator();
+
+// const theme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     background: "transparent",
+//   },
+// };
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up Riki Gaal dance</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer theme={theme}>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Home"
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+};
+export default App;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
